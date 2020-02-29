338ff5bdaa47056712add6ea35105c079b934b04
Grzegorz Pawlaszek <s17651@pjwstk.edu.pl>
2020-02-29 18:03:49 +0100

    skonczone

--- cwiczenia1/cwiczenia1/Program.cs
+++ cwiczenia1/cwiczenia1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace cwiczenia1
@@ -8,11 +9,19 @@ namespace cwiczenia1
     {
         public static async Task Main(string[] args)
         {
+            //asdasd
             var client = new HttpClient();
             var result = await client.GetAsync("https://www.pja.edu.pl");
 
             if (result.IsSuccessStatusCode) {
                 var html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+
+                foreach( var m in matches){
+                    Console.WriteLine(m.ToString());
+                }
             }
         }
     }
