5cf5582ad1b38903e029eccd0f4997a7c347e811
Grzegorz Pawlaszek <s17651@pjwstk.edu.pl>
2020-02-29 17:48:34 +0100

    zmiana

---
+++ cwiczenia1/cwiczenia1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace cwiczenia1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode) {
+                var html = await result.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
